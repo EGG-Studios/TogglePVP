d267bc1a2c353adc6e9b31f7eb3f7cbd251e78c3
Foxie <fabianverhagen2011@gmail.com>
2025-07-29 23:15:10 +0200

    Initial Commit

---
+++ src/main/kotlin/org/eggstudios/togglePVP/TogglePVP.kt
@@ -0,0 +1,10 @@
+package org.eggstudios.togglePVP
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TogglePVP : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+}
