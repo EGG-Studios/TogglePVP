27a64763d039f9ec081a19b932372a57bf7fe550
Foxie <fabianverhagen2011@gmail.com>
2025-09-02 14:49:46 +0200

    Added team logic

--- src/main/kotlin/org/eggstudios/togglePVP/TogglePVP.kt
+++ src/main/kotlin/org/eggstudios/togglePVP/TogglePVP.kt
@@ -1,5 +1,6 @@
 package org.eggstudios.togglePVP
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 import org.eggstudios.togglePVP.commands.PVPCommand
 
@@ -7,6 +8,7 @@ class TogglePVP : JavaPlugin() {
 
     override fun onEnable() {
         registerCommand()
+        createTeams()
 
         logger.info("TogglePVP has been enabled.")
     }
@@ -14,5 +16,19 @@ class TogglePVP : JavaPlugin() {
     fun registerCommand() {
         getCommand("pvp")?.setExecutor(PVPCommand())
         logger.info("Commands have been registered successfully.")
+    }
+
+    fun createTeams() {
+        val scoreboard = Bukkit.getScoreboardManager().mainScoreboard
+
+        if (scoreboard.getTeam("PVP") == null) {
+            val pvpTeam = scoreboard.registerNewTeam("PVP")
+            // Make the name of the team red
+        }
+
+        if (scoreboard.getTeam("PVPLO") == null) {
+            val pvpLoTeam = scoreboard.registerNewTeam("PVPLO")
+            // Make the name of the team dark red
+        }
     }
 }
--- src/main/kotlin/org/eggstudios/togglePVP/commands/PVPCommand.kt
+++ src/main/kotlin/org/eggstudios/togglePVP/commands/PVPCommand.kt
@@ -1,12 +1,16 @@
 package org.eggstudios.togglePVP.commands
 
-import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import java.util.HashMap
+
 
 class PVPCommand : CommandExecutor {
+    var cooldown: HashMap<Player?, Long?> = HashMap<Player?, Long?>()
+
     override fun onCommand(
         p0: CommandSender,
         p1: Command,
@@ -15,15 +19,39 @@ class PVPCommand : CommandExecutor {
     ): Boolean {
         if (p0 is Player) {
             if (!p3.isEmpty()) {
-                if (p3[0] == "on") {
-                    TODO("Add logic to turn the players name red")
-//                    return true (Uncomment this line when logic is added)
-                } else if (p3[0] == "off") {
-                    TODO("Add logic to turn the players name back to normal")
+                if (checkCooldown(p0.player)) {
+                    if (p3[0] == "pvp") {
+                        val team = Bukkit.getScoreboardManager().mainScoreboard.getTeam("PVP")
+                        team?.addEntry(p0.name)
+
+                    return true
+                    } else if (p3[0] == "loot") {
+                        val team = Bukkit.getScoreboardManager().mainScoreboard.getTeam("PVPLO")
+                        team?.addEntry(p0.name)
+                    }
+                }
+                if (p3[0] == "off") {
+                    p0.player?.scoreboard?.getTeam("PVP")?.removeEntry(p0.name)
+                    p0.player?.scoreboard?.getTeam("PVPLO")?.removeEntry(p0.name)
+                    cooldown[p0.player] = System.currentTimeMillis()
                 }
             }
         }
 
+        return false
+    }
+
+    fun checkCooldown(player: Player?): Boolean {
+        if (cooldown.contains(player)) {
+            val secondsLeft = (cooldown[player]!! + 3_600_000 - System.currentTimeMillis()) / 1000 // 24 hour cooldown
+            if (secondsLeft > 0) {
+                player?.sendMessage("You must wait $secondsLeft seconds before toggling PVP again.")
+                return true
+            }
+            else {
+                cooldown.remove(player)
+            }
+        }
         return false
     }
 }
